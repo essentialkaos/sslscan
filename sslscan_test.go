546ddf198aa6dead7f881287c6ebf594e05d66c4
Anton Novojilov <andy@essentialkaos.com>
2017-02-27 11:09:47 -0500

    Updated test for compatibility with latest version

--- sslscan_test.go
+++ sslscan_test.go
@@ -33,7 +33,7 @@ func (s *SSLLabsSuite) TestInfo(c *check.C) {
 	c.Assert(api, check.NotNil)
 	c.Assert(err, check.IsNil)
 
-	c.Assert(api.Info.EngineVersion, check.Equals, "1.27.0")
+	c.Assert(api.Info.EngineVersion, check.Equals, "1.27.3")
 	c.Assert(api.Info.CriteriaVersion, check.Equals, "2009m")
 }
 
